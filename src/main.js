2c4858f8480c87b0df042805c881cc6531ad5019
ywc8851 <60125497+ywc8851@users.noreply.github.com>
2021-01-23 16:47:18 +0900

    fundamental

--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
 // Fetch the items from the JSON file
 function loadItems() {
-  return fetch('data/data.json')
-    .then(response => response.json())
-    .then(json => json.items);
+  return fetch("data/data.json") // data.json에서 데이터 받아오기
+    .then((response) => response.json())
+    .then((json) => json.items); // 성공하면 json안의 items를 return
 }
 
 // Update the list with the given items
 function displayItems(items) {
-  const container = document.querySelector('.items');
-  container.innerHTML = items.map(item => createHTMLString(item)).join('');
+  const container = document.querySelector(".items");
+  // 받아온 items를 html로 변환 : map을 이용
+  container.innerHTML = items.map((item) => createHTMLString(item)).join(""); // join을 이용해서 하나의 문자열로
 }
 
 // Create HTML list item from the given data item
@@ -30,19 +31,19 @@ function onButtonClick(event, items) {
     return;
   }
 
-  displayItems(items.filter(item => item[key] === value));
+  displayItems(items.filter((item) => item[key] === value));
 }
 
 function setEventListeners(items) {
-  const logo = document.querySelector('.logo');
-  const buttons = document.querySelector('.buttons');
-  logo.addEventListener('click', () => displayItems(items));
-  buttons.addEventListener('click', event => onButtonClick(event, items));
+  const logo = document.querySelector(".logo");
+  const buttons = document.querySelector(".buttons");
+  logo.addEventListener("click", () => displayItems(items));
+  buttons.addEventListener("click", (event) => onButtonClick(event, items));
 }
 
 // main
 loadItems()
-  .then(items => {
+  .then((items) => {
     displayItems(items);
     setEventListeners(items);
   })
